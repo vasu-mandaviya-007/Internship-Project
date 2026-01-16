2234c0fdadbc139cc641dda609db31186d14da9b
vasu-mandaviya-007 <vasumandaviya@gmail.com>
2026-01-16 13:00:49 +0530

    Second Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,11 @@ import React, { useEffect } from 'react'
 const App = () => {
 
     useEffect(() => {
-        fetch("http://localhost:8000/").then((res) => res.json()).then(data => console.log(data))
+        fetch("http://localhost:8000/").then((res) => res.json()).then(data => alert(data.message))
     }, [])
 
     return (
         <div>
-
         </div>
     )
 }
